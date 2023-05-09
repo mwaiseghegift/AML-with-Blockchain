a27ea8b2213a9f656d88ca3277b4e3eb0a358a83
itsregalo <itsregalo047@gmail.com>
2023-05-09 14:40:25 +0300

    order by location

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def IndexView(request):
-    return render(request, "index.html")
+    return render(request, "dashboard.html")
 
 def Dashboard(request):
     return render(request, 'dashboard.html')
