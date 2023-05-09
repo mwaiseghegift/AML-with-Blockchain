a579699841f6f89b50ad0b11672dace9d5941b82
itsregalo <itsregalo047@gmail.com>
2023-05-09 17:34:39 +0300

    order by location

--- payments/views.py
+++ payments/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render
 from .models import EthereumTransaction
 from .utils import get_latest_transactions
+from django.core.paginator import Paginator
 
 def transaction_list(request):
     transactions = get_latest_transactions()
@@ -11,6 +12,11 @@ def transaction_list(request):
 
 def transactions(request):
     transactions = EthereumTransaction.objects.order_by('-timestamp')
+
+    # pagination
+    paginator = Paginator(transactions, 100)
+    page = request.GET.get('page')
+    transactions = paginator.get_page(page)
 
     context = {
         'transactions': transactions
