62ebed15ba83b5c255d563ee355466dfbf6e2131
itsregalo <itsregalo047@gmail.com>
2023-05-09 15:10:27 +0300

    transactions

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import transaction_list
+from .views import *
 
 app_name = 'payments'
 
 urlpatterns = [
     path('transactions/', transaction_list, name='transactions'),
+    path('transactions-list/', transactions, name='transactions-list'),
+
 ]
--- payments/views.py
+++ payments/views.py
@@ -7,3 +7,13 @@ from .utils import get_latest_transactions
 def transaction_list(request):
     transactions = get_latest_transactions()
     return render(request, 'transaction_list.html', {'transactions': transactions})
+
+
+def transactions(request):
+    transactions = EthereumTransaction.objects.order_by('-timestamp')
+
+    context = {
+        'transactions': transactions
+    }
+
+    return render(request, 'transactions.html', context)
